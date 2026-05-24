d7da2acd3618c5f7d5f41646b36789c1f8814d0b
S SOMU <91591192+AYMPONSOMU@users.noreply.github.com>
2026-05-24 21:03:34 +0530

    Update game.js

--- game.js
+++ game.js
@@ -2,7 +2,26 @@ const enterBtn = document.getElementById("enterBtn");
 
 enterBtn.addEventListener("click", () => {
 
-  // go to Realm 1
-  window.location.href = "realms/realm1-agni/index.html";
+  // Show welcome message
+  document.body.innerHTML = `
+    <div style="
+      height:100vh;
+      display:flex;
+      justify-content:center;
+      align-items:center;
+      background:black;
+      color:orange;
+      font-size:30px;
+      text-align:center;
+    ">
+      Welcome to AYMPON 144 🌌<br><br>
+      Entering Mystic Realm...
+    </div>
+  `;
+
+  // After 2 seconds go to realm
+  setTimeout(() => {
+    window.location.href = "realms/realm1-agni/index.html";
+  }, 2000);
 
 });
